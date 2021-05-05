3a33da40f78d4a8764676f6f001c288819f62d40
fedyashevvic <fedyashevvic@gmail.com>
2021-05-05 19:39:47 +0300

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import Config from 'config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-
+  app.enableCors();
   let server: any = Config.get('server')
 
   await app.listen(server.port)
